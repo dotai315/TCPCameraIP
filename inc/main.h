6ab72aa88528462114f67d865dd1682ed8a3a681
Đỗ Hữu Tài <57071897+dotai315@users.noreply.github.com>
2022-10-04 15:27:28 +0700

    Update main.h

--- inc/main.h
+++ inc/main.h
@@ -1,13 +1,19 @@
 #include "videoCapture.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
+#include <stdbool.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netdb.h>
+
 #include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
 #define BUFFLEN 1024
